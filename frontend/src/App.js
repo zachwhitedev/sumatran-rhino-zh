f3d36542f78dcd09cbbbf7ee163bd265b6a08421
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-11-09 14:35:25 -0800

    initial push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Zoohackathon
-      </h1>
+    <div>
+      <div className="App">
+        <h1>
+          hello world
+       </h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div col-sm-6>
+          {/* <img id='universe' src="textBubble.png" alt="" className=""> */}
+          </div>
+      </div>
+    </div>
     </div>
   );
 }
